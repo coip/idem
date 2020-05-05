854f4f03523ad56ab35883e25de4ff4353bbc6b1
ethan c <e@coip.me>
2020-05-05 17:03:18 -0500

    cmd.Path might help...

--- idem.go
+++ idem.go
@@ -16,6 +16,15 @@ var (
 
 func init() {
 	flag.Parse()
+	//verify git, from coip/gitgopher
+	if srcgetter, err := exec.LookPath(getIdemSHA.Args[0]); err != nil {
+		os.Stderr.Write([]byte(err.Error()))
+	} else {
+		if *verbose {
+			os.Stderr.Write([]byte("plumbing for version: using " + getIdemSHA.Args[0] + "@" + srcgetter))
+		}
+		getIdemSHA.Path = srcgetter
+	}
 }
 
 //GetCurrentMainGopher will return a string which attempts to indicate:
